a1a95a7b1a0eea336b6bef6a367c36626be1a887
Eugene St <estebelsky@gmail.com>
2022-12-15 18:49:13 +0300

    Homework to define all evens in range is completed

---
+++ evenInRange/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter any positive number:");
+    int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    //string ifEven = (N % 2 == 0) ? "Even" : "Odd";
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 == 0)
+        {
+        Console.Write(" " + i);
+        }
+    }
+
+
