3ff80fbfe7b70dc90d95d544c5a6db6f9f9410c8
Eugene St <estebelsky@gmail.com>
2022-12-15 07:28:29 +0300

    fraction became an random sum

--- fraction/Program.cs
+++ fraction/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = new Random().Next(1,10);
+﻿// it was fraction of two numbers before
+int numberA = new Random().Next(1,10);
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
