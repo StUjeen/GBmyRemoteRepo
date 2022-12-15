3d9021141894595f9cb18be0390067e88892c53e
Eugene St <estebelsky@gmail.com>
2022-12-15 07:41:00 +0300

    hello Masha programm created

---
+++ helloMasha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+if (name == "Masha")
+    {
+    Console.Write("Hello, my dear ");
+    Console.WriteLine(name);
+    } 
+else
+    {
+    Console.Write("Hi, ");
+    Console.WriteLine(name);        
+    }
