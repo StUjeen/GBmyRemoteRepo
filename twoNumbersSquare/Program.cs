fa4e2795ab5d4e901e6d8788b998601c6a0737d1
Eugene St <estebelsky@gmail.com>
2022-12-15 13:36:39 +0300

    Test if first input number is a square of the second input number programm created

---
+++ twoNumbersSquare/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter first number:");
+    int numFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+    int numSecond = Convert.ToInt32(Console.ReadLine());
+
+string result = (numFirst == numSecond*numSecond) ? "YES": "NO";
+Console.WriteLine("Is the first number a square of the second one? " + result);
