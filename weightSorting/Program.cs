12bdc9061556ba23294d0bc39825947696cb90c6
Eugene St <estebelsky@gmail.com>
2022-12-15 08:17:34 +0300

    weight sorting programm looped

---
+++ weightSorting/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] weight = {1,8,3,2,6};
+int i = 0;
+int max = weight[0];
+
+    while (i < 5)
+    {
+        if (weight[i] < max) 
+        {
+           i++;
+        } else
+        {
+            max = weight[i];
+            i++;
+        }
+    }
+
+Console.WriteLine(max);
