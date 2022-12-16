6edb44b1852f03bc1a10dc552f6883a04e71bdc6
Eugene St <estebelsky@gmail.com>
2022-12-16 03:28:08 +0300

    square project updated - (int)method() - declare of resulting data type

--- square/Program.cs
+++ square/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Enter any number");
 int x = Convert.ToInt32(Console.ReadLine() ?? "");
-int result = x * x;
+int result = (int)Math.Pow(x, 2);
 Console.WriteLine($"{x} -> {result}");
