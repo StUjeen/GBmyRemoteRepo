5cdb58c3761257edc97733d5287553c3638c2064
Eugene St <estebelsky@gmail.com>
2022-12-16 04:08:31 +0300

    Homework evenOdd updated with $ construction

--- evenOdd/Program.cs
+++ evenOdd/Program.cs
@@ -1,6 +1,5 @@
 ﻿Console.WriteLine("Enter any number:");
     int number = Convert.ToInt32(Console.ReadLine() ?? "");
-
     string result = (number % 2 == 0) ? "Even" : "Odd"; 
-    Console.WriteLine("The number " + number + " is " + result);
+    Console.WriteLine($"The number {number} is {result}");
     
