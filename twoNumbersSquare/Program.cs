314f99e1976afa57f19a57c42dcf150c6be0d223
Eugene St <estebelsky@gmail.com>
2022-12-16 03:50:27 +0300

    twoNumbersSquare updated with mat.pow method

--- twoNumbersSquare/Program.cs
+++ twoNumbersSquare/Program.cs
@@ -3,5 +3,5 @@
 Console.WriteLine("Enter second number:");
     int numSecond = Convert.ToInt32(Console.ReadLine() ?? "");
 
-string result = (numFirst == numSecond*numSecond) ? "YES": "NO";
+string result = (numFirst == Math.Pow(numSecond, 2)) ? "YES": "NO";
 Console.WriteLine("Is the first number a square of the second one? " + result);
