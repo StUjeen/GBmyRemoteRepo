a95ef23f9094b0f89682968620ef05e6e9dec196
Eugene St <estebelsky@gmail.com>
2022-12-17 14:24:09 +0300

    find array method created

---
+++ findInArrayMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿// create array method
+void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index<length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+// print array method
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+//input data
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
