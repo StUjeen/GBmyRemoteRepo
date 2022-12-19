0c8d0bb9f695369c71d5ffcdb0c538eff6e87d8c
Eugene St <estebelsky@gmail.com>
2022-12-19 23:07:09 +0300

    Homework to find second entry from three digit number has been completed

---
+++ secNumThreeDigit/Program.cs
@@ -0,0 +1,12 @@
+﻿//enter and test input data 
+int number;
+while (true)
+{
+    Console.WriteLine("Enter three digit number:");
+    number = Convert.ToInt32(Console.ReadLine() ?? "");
+    if (number > 99 && number < 1000)
+        break;
+    Console.WriteLine("Wrong number entered:");
+}
+ int result =  (number / 10) % 10;
+Console.WriteLine($"{number} -> {result}");
