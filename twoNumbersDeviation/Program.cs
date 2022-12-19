30b083700963158f93eb78603f933c1aefe4ca7f
Eugene St <estebelsky@gmail.com>
2022-12-19 21:40:34 +0300

    two numbers deviation and deviation to 7 and 23 projects added

---
+++ twoNumbersDeviation/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число 1: ");
+string input1 = Console.ReadLine() ?? "";
+Console.WriteLine("Введите второе число 2: ");
+string input2 = Console.ReadLine() ?? "";
+
+int number1 = int.Parse(input1);
+int number2 = int.Parse(input2);
+
+if (number1 % number2 == 0)
+{
+    Console.WriteLine($"{number1}, {number2} -> кратно");
+}
+else if (number2 % number1 != 0)
+{
+    Console.WriteLine($"{number1}, {number2} -> не кратно, остаток {number2 % number1}");
+}
