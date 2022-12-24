376a0275ce5d0c704b812d074b15c2d3ba64d40a
Eugene St <estebelsky@gmail.com>
2022-12-24 22:29:12 +0300

    homework for practice three added

---
+++ cubeFrom1toN/Program.cs
@@ -0,0 +1,21 @@
+﻿int n;
+
+Console.Write("Enter N: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+
+//logic
+Console.Write($"{n} -> ");
+for (int i = 1; i <= n; i++)
+{
+    if (i < n)
+    {
+        int sqrtI = Convert.ToInt32(Math.Pow(i, 3));
+        Console.Write($"{sqrtI}, ");
+    }
+    else
+    {
+        int sqrtI = Convert.ToInt32(Math.Pow(i, 3));
+        Console.Write($"{sqrtI}.");
+    }
+}
