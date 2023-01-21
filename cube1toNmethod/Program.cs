3ad03a7f3e98100b1e6c8a348d5326bead9f4fbf
Eugene St <estebelsky@gmail.com>
2023-01-21 14:08:01 +0300

    Lesson 3 Function continue Array Sorting from min to max from max to min

---
+++ cube1toNmethod/Program.cs
@@ -0,0 +1,28 @@
+﻿int n;
+
+Console.Write("Enter N: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{n} -> ");
+cubeRow(ref n);
+
+
+//logic
+static string cubeRow (ref int n)
+{
+    
+    for (int i = 1; i <= n; i++)
+    {
+        if (i < n)
+        {
+            int sqrtI = Convert.ToInt32(Math.Pow(i, 3));
+            Console.Write($"{sqrtI}, ");
+        }
+        else
+        {
+            int sqrtI = Convert.ToInt32(Math.Pow(i, 3));
+            Console.Write($"{sqrtI}.");
+        }
+    }
+    return;
+}
---
+++ l3funcHomeWork/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int[] arr = { 1, 0, 6, 3, 9, 5, 7, 2, 8, 4 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        //                 i  j
+        //int[] arr = { 9, 0, 1, 3, 6, 5, 7, 2, 8 };
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temp;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
