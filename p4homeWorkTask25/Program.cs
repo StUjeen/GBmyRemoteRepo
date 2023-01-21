45c682a897ad2defe5d0d0b663bd82cd2d8bf0f0
Eugene St <estebelsky@gmail.com>
2023-01-21 18:29:25 +0300

    practice 4 home work task29 completed

---
+++ p4homeWorkTask25/Program.cs
@@ -0,0 +1,50 @@
+﻿///////////////////////////////////////////////////////
+//////// Произведение чисел от 1 до N /////////////////
+///////////////////////////////////////////////////////
+// Сначала описываем этапы выполнения программы
+// три этапа:
+// 1 ввод данных
+// 2 вычисление
+// 3 вывод данных
+/////////ТЕЛО ПРОГРАММЫ//////////////
+//*******1 ВВОД ДАННЫХ*********
+Console.Clear();
+for (int i = 0; i < 8; i++)
+{
+int[] num = GetArrayFromUser($"Enter array element [{i}]: ","Input Error!");
+}
+
+//*******2 ВЫЧИСЛЕНИЕ**********
+//int productNumbers = GetProductNumbers(num);
+//*******3 ВЫВОД ДАННЫХ*********
+Console.Write($"[");
+Console.Write(num);
+Console.Write($"]");
+///////////////МЕТОДЫ/////////////////
+//*******1 ВВОД ДАННЫХ*********
+
+int GetArrayFromUser(string message, string errorMeassage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMeassage);
+
+    }
+}
+
+//*******2 ВЫЧИСЛЕНИЕ**********
+/*int GetProductNumbers(int number)
+{
+    int product = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        product *= i;
+    }
+    return product;
+
+}
+*/
---
+++ p4homeWorkTask29/Program.cs
@@ -0,0 +1,60 @@
+﻿///////////////////////////////////////////////////////
+//////// Произведение чисел от 1 до N /////////////////
+///////////////////////////////////////////////////////
+// Сначала описываем этапы выполнения программы
+// три этапа:
+// 1 ввод данных
+// 2 вычисление
+// 3 вывод данных
+/////////ТЕЛО ПРОГРАММЫ//////////////
+//*******1 ВВОД ДАННЫХ*********
+Console.Clear();
+int[] inputArray = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    int num = GetArrayFromUser($"Enter array element [{i}]: ", "Input Error!");
+    inputArray[i] = num;
+}
+//*******2 ВЫЧИСЛЕНИЕ**********
+//*******3 ВЫВОД ДАННЫХ*********
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (i == inputArray.Length - 1)
+    {
+        Console.Write($"{inputArray[i]} -> ");
+    }
+    else
+    {
+        Console.Write($"{inputArray[i]},");
+    }
+
+}
+Console.Write($"[");
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (i == inputArray.Length - 1)
+    {
+        Console.Write($"{inputArray[i]}");
+    }
+    else
+    {
+        Console.Write($"{inputArray[i]},");
+    }
+
+}
+Console.Write($"]");
+///////////////МЕТОДЫ/////////////////
+//*******1 ВВОД ДАННЫХ*********
+
+int GetArrayFromUser(string message, string errorMeassage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMeassage);
+
+    }
+}
