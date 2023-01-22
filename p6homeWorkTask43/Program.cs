14f2ad4401c1690f631f7ff06d6cd4ae418502b0
Eugene St <estebelsky@gmail.com>
2023-01-22 15:02:16 +0300

    p6HWTask43 iterative GetNumbersFromUser method modified, PrintArray method modified to print in given format

--- p6homeWorkTask43/Program.cs
+++ p6homeWorkTask43/Program.cs
@@ -7,34 +7,43 @@ b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 //*******1 ВВОД ДАННЫХ*********
 Console.Clear();
 
-int k1 = GetNumbersFromUser($"Enter k1 (y = k1 * x + b1)", "Input Error!");
-int b1 = GetNumbersFromUser($"Enter b1 (y = k1 * x + b1)", "Input Error!");
-int k2 = GetNumbersFromUser($"Enter k2 (y = k2 * x + b2)", "Input Error!");
-int b2 = GetNumbersFromUser($"Enter b2 (y = k2 * x + b2)", "Input Error!");
-
+string[,] parameters = GetNumbersFromUser($"Enter parameters ", "Input Error!");
 //*******2 ВЫЧИСЛЕНИЕ**********
-double[] crossPoint = GetCrossPoint(k1, b1, k2, b2);
+double[] crossPoint = GetCrossPoint(parameters);
 //*******3 ВЫВОД ДАННЫХ*********
-PrintArray(crossPoint);
+PrintArray(crossPoint, parameters);
 
 ///////////////МЕТОДЫ/////////////////
 //*******1 ВВОД ДАННЫХ*********
-int GetNumbersFromUser(string message, string errorMeassage)
+string[,] GetNumbersFromUser(string message, string errorMeassage)
 {
-    while (true)
+    var inputArray = new string[2, 4] { { "b1", "k1", "b2", "k2" }, { "", "", "", "" } };
+    for (int i = 0; i < 4; i++)
     {
-        Console.WriteLine(message);
-        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
-        if (isCorrect)
-            return userNumber;
-        Console.WriteLine(errorMeassage);
+
+        while (true)
+        {
+            Console.WriteLine($"{message} {inputArray[0, i]}");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+            if (isCorrect)
+                inputArray[1, i] = Convert.ToString(userNumber);
+            break;
+            Console.WriteLine(errorMeassage);
+        }
     }
+    return inputArray;
 }
 
 
 //*******2 ВЫЧИСЛЕНИЕ**********
-double[] GetCrossPoint(int k1, int b1, int k2, int b2)
+double[] GetCrossPoint(string[,] parameters)
 {
+    //double[] paramArray = Convert.ToDouble(parameters[1,]);
+    double b1 = Convert.ToDouble(parameters[1, 0]);
+    double k1 = Convert.ToDouble(parameters[1, 1]);
+    double b2 = Convert.ToDouble(parameters[1, 2]);
+    double k2 = Convert.ToDouble(parameters[1, 3]);
+
     double x = -(b1 - b2) / (k1 - k2);
     double y = k2 * x + b2;
     double[] crossPoint = new double[2];
@@ -44,7 +53,25 @@ double[] GetCrossPoint(int k1, int b1, int k2, int b2)
 }
 
 //*********3 ВЫВОД ДАННЫХ*********
-void PrintArray(double[] array)
+void PrintArray(double[] array, string[,] parameters)
 {
-    Console.Write($"k1={k1}, b1={b1}, k2={k2}, b2={b2} -> ({array[0]}, {array[1]})");
+    int len = parameters.Length / 2;
+
+    for (int j = 0; j < len; j++)
+        for (int i = 0; i < 2; i++)
+        {
+            if (i == 1 && j == len - 1)
+            {
+                Console.Write($"{parameters[i, j]} ");
+            }
+            else if (i == 1 && j < len - 1)
+            {
+                Console.Write($"{parameters[i, j]}, ");
+            }
+            else if (i == 0)
+            {
+                Console.Write($"{parameters[i, j]} = ");
+            }
+        }
+    Console.Write($" -> ({array[0]}, {array[1]})");
 }
