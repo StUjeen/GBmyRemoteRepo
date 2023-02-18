beea9b705b4f59b4e50989dff802f45374669317
Eugene St <estebelsky@gmail.com>
2023-02-18 12:20:31 +0300

    p9homeWorkTask64 complete - recursion to print numbers from N to 1

---
+++ p8inclassTask57/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.*/
+
+//создание массива
+int m = GetUserData("Enter m: ", "Error!");
+int n = GetUserData("Enter n: ", "Error!");
+int minVal = GetUserData("Enter minValue: ", "Error!");
+int maxVal = GetUserData("Enter maxValue: ", "Error!");
+//логика
+///Создать массив
+int[] array = GetArray(m, n, minVal, maxVal);
+///подсчитать количество элементов
+string result = GetCountOfEntries(array);
+//вывод результата на экран
+PrintArray(result);
+
+//1 Ввод данных
+int GetUserData(string message, string errorMeassage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMeassage);
+
+    }
+}
+//2 Логика
+int[,] GetArray(int m, int n, int minValue, int maxValues)
+{
+    int[,] array = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = rand.Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+string[] GetEntriesCount(int[] array)
+{
+    string[] count = new string[];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+             
+        }
+    }
+}
+
+
+//3 Вывод данных
---
+++ p9homeWorkTask64/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+//*******1 ВВОД ДАННЫХ*********
+Console.Clear();
+
+int N = GetNumberFromUser($"Enter N: ", "Input Error!");
+//*******2 ВЫЧИСЛЕНИЕ**********
+Console.Write($"N = {N} -> '{GetSequence(N)}'");
+//*******3 ВЫВОД ДАННЫХ*********
+//PrintArray(crossPoint, parameters);
+
+///////////////МЕТОДЫ/////////////////
+//*******1 ВВОД ДАННЫХ*********
+int GetNumberFromUser(string message, string errorMeassage)
+{
+    while (true)
+        {
+            Console.Write($"{message}");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+            if (isCorrect)
+                return userNumber;
+            Console.WriteLine(errorMeassage);
+        }   
+}
+
+string GetSequence(int number) 
+{
+    if (number == 1) return Convert.ToString(number);
+     return number + ", " + GetSequence(number - 1);
+}
